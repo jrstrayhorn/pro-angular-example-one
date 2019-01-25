370a657a2b0de0214371b213ddb1998a3fc7658d
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-25 15:11:44 -0500

    done with chapter 13

--- src/app/component.ts
+++ src/app/component.ts
@@ -18,6 +18,35 @@ export class ProductComponent {
     return this.model.getProducts()[position];
   }
 
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
+  }
+
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+
+  getProductCount(): number {
+    console.log("getProductCount invoked");
+    return this.getProducts().length;
+  }
+
+  targetName: string = "Kayak";
+
+  counter: number = 1;
+
+  get nextProduct(): Product {
+    return this.model.getProducts().shift();
+  }
+
+  getProductPrice(index: number): number {
+    return Math.floor(this.getProduct(index).price);
+  }
+
+  getKey(index: number, product: Product) {
+    return product.id;
+  }
+
   getClassesByPosition(position: number): string {
     let product = this.getProductByPosition(position);
     return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
