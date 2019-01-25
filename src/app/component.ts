94b11e8679f8d1efc64246ddf1b14742022ea779
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-25 15:21:05 -0500

    getting clean

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,4 +1,4 @@
-import { Component, ApplicationRef } from "@angular/core";
+import { Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
 
@@ -9,75 +9,11 @@ import { Product } from "./product.model";
 export class ProductComponent {
   model: Model = new Model();
 
-  constructor(ref: ApplicationRef) {
-    (<any>window).appRef = ref;
-    (<any>window).model = this.model;
-  }
-
-  getProductByPosition(position: number): Product {
-    return this.model.getProducts()[position];
-  }
-
   getProduct(key: number): Product {
     return this.model.getProduct(key);
   }
 
   getProducts(): Product[] {
     return this.model.getProducts();
-  }
-
-  getProductCount(): number {
-    console.log("getProductCount invoked");
-    return this.getProducts().length;
-  }
-
-  targetName: string = "Kayak";
-
-  counter: number = 1;
-
-  get nextProduct(): Product {
-    return this.model.getProducts().shift();
-  }
-
-  getProductPrice(index: number): number {
-    return Math.floor(this.getProduct(index).price);
-  }
-
-  getKey(index: number, product: Product) {
-    return product.id;
-  }
-
-  getClassesByPosition(position: number): string {
-    let product = this.getProductByPosition(position);
-    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
-  }
-
-  getClasses(): string {
-    return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
-  }
-
-  getClassesByKey(key: number): string {
-    let product = this.model.getProduct(key);
-    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
-  }
-
-  getClassMap(key: number): Object {
-    let product = this.model.getProduct(key);
-    return {
-      "text-center bg-danger": product.name == "Kayak",
-      "bg-info": product.price < 50
-    };
-  }
-
-  fontSizeWithUnits: string = "30px";
-  fontSizeWithoutUnits: string = "30";
-
-  getStylesMap(key: number): Object {
-    let product = this.model.getProduct(key);
-    return {
-      fontSize: "30px",
-      "margin.px": 100,
-      color: product.price > 50 ? "red" : "green"
-    };
   }
 }
