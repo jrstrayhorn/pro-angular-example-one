204b1937f6f20d94b83967dffdaa2958e361b927
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-30 10:53:04 -0500

    done with chapter 14

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
+import { NgForm } from "@angular/forms";
+import { ProductFormGroup } from "./form.model";
 
 @Component({
   selector: "app",
@@ -8,6 +10,7 @@ import { Product } from "./product.model";
 })
 export class ProductComponent {
   model: Model = new Model();
+  form: ProductFormGroup = new ProductFormGroup();
 
   getProduct(key: number): Product {
     return this.model.getProduct(key);
@@ -33,28 +36,15 @@ export class ProductComponent {
     console.log("New Product: " + this.jsonProduct);
   }
 
-  getValidationMessages(state: any, thingName?: string) {
-    let thing: string = state.path || thingName;
-    let messages: string[] = [];
-    if (state.errors) {
-      for (let errorName in state.errors) {
-        switch (errorName) {
-          case "required":
-            messages.push(`You must enter a ${thing}`);
-            break;
-          case "minlength":
-            messages.push(
-              `A ${thing} must be at least ${
-                state.errors["minlength"].requiredLength
-              } characters`
-            );
-            break;
-          case "pattern":
-            messages.push(`The ${thing} contains illegal characters`);
-            break;
-        }
-      }
+  formSubmitted: boolean = false;
+
+  submitForm(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.formSubmitted = false;
     }
-    return messages;
   }
 }
